6c4da1d62b9df9d9788b981cbda8e5cb721896b9
katou440 <2101340672go@cyber-u.ac.jp>
2023-11-29 19:35:35 +0900

    Add mongodb access

--- routes/notes.js
+++ routes/notes.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 // 接続情報を設定
 const { MongoClient } = require("mongodb");
-const uri = "***";
+const uri = "mongodb+srv://a-katou:***********@a-katou.n9k5w3f.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri);
 router.get('/', async (req, res) => {
 // データベース、コレクションを指定
